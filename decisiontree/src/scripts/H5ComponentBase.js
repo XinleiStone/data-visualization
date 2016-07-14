26762076e87d63444eb4fec8d6c2a54edffac539
XinleiStone <741447430@qq.com>
2016-07-14 23:01:12 +0800

    rewrite decision tree

---
+++ decisiontree/src/scripts/H5ComponentBase.js
@@ -0,0 +1,46 @@
+/* 基本图文组件对象 */
+var H5ComponentBase = function(name, cfg) {
+	"use strict";
+	var className = "h5_component_" + name;
+	var id = ('h5_component_base_' + Math.random()).replace('.', '_');
+	var $component = $('<div id='+ id +' class="h5_component_base h5_component '+ className +'"></div>');
+
+	cfg.text && $component.text(cfg.text);
+	cfg.width && $component.width(cfg.width / 2);
+	cfg.height && $component.height(cfg.height / 2);
+	cfg.css && $component.css(cfg.css);
+	cfg.bg && $component.css('backgroundImage', 'url('+ cfg.bg +')');
+
+	if (cfg.center === true) {
+		$component.css({
+			marginLeft: cfg.width/4 * -1 + 'px',
+			left: '50%'
+		});
+	}
+
+	$component.on("onLoad", function() {
+		//$(this).fadeIn();
+		$component.addClass(className + '_load').removeClass(className + '_leave');
+		cfg.animateIn && $component.animate(cfg.animateIn);
+		$component.css('display', 'block');
+		return false;
+	});
+
+	$component.on("onLeave", function() {
+		//$(this).fadeOut();
+		$component.addClass(className + '_leave').removeClass(className + '_load');
+		cfg.animateOut && $component.animate(cfg.animateOut, function() {
+			$component.css('display', 'none');
+		});
+		
+		return false;
+	});
+
+	var leave = false;
+	$('body').click(function() {
+		leave = !leave;
+		$('.h5_component_base').trigger(leave ? 'onLoad': 'onLeave');
+	});
+
+	return $component;
+};
